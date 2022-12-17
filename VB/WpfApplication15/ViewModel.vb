ddc2e3425c62b90b262fad711fa84241ed49570d
DevExpressExampleBot <fakeEmail>
2022-12-17 06:59:59 +0400

    Source auto update [skip ci]

--- VB/WpfApplication15/ViewModel.vb
+++ VB/WpfApplication15/ViewModel.vb
@@ -1,48 +1,50 @@
-﻿Imports System
-Imports System.Linq
 Imports System.ComponentModel
 Imports System.Data
+
 Namespace WpfApplication15
+
     Public Class ViewModel
         Implements INotifyPropertyChanged
 
         Private DataSet As DataSet
+
         Private _masterSource As Object
+
         Private _detailSource As Object
+
         Private _currentRow As DataRowView
 
-        Public Property MasterSource() As Object
+        Public Property MasterSource As Object
             Get
                 Return _masterSource
             End Get
+
             Private Set(ByVal value As Object)
-                If _masterSource Is value Then
-                    Return
-                End If
+                If _masterSource Is value Then Return
                 _masterSource = value
                 RaisePropertyChanged("MasterSource")
             End Set
         End Property
-        Public Property DetailSource() As Object
+
+        Public Property DetailSource As Object
             Get
                 Return _detailSource
             End Get
+
             Private Set(ByVal value As Object)
-                If _detailSource Is value Then
-                    Return
-                End If
+                If _detailSource Is value Then Return
                 _detailSource = value
                 RaisePropertyChanged("DetailSource")
             End Set
         End Property
-        Public Property CurrentRow() As DataRowView
+
+        Public Property CurrentRow As DataRowView
             Get
                 Return _currentRow
             End Get
+
             Set(ByVal value As DataRowView)
-                If _currentRow Is value Then
-                    Return
-                End If
+                If _currentRow Is value Then Return
                 _currentRow = value
                 RaisePropertyChanged("CurrentRow")
                 UpdateDetailSource()
@@ -55,14 +57,13 @@ Namespace WpfApplication15
         End Sub
 
         Private Function CreateData() As DataSet
-            Dim mdt As New DataTable("Company")
+            Dim mdt As DataTable = New DataTable("Company")
             mdt.Columns.Add(New DataColumn("Name", GetType(String)))
             mdt.Columns.Add(New DataColumn("ID", GetType(Integer)))
             mdt.Rows.Add("Ford", 4)
             mdt.Rows.Add("Nissan", 5)
             mdt.Rows.Add("Mazda", 6)
-
-            Dim ddt As New DataTable("Models")
+            Dim ddt As DataTable = New DataTable("Models")
             ddt.Columns.Add(New DataColumn("Name", GetType(String)))
             ddt.Columns.Add(New DataColumn("MaxSpeed", GetType(Integer)))
             ddt.Columns.Add(New DataColumn("CompanyName", GetType(String)))
@@ -70,22 +71,22 @@ Namespace WpfApplication15
             ddt.Rows.Add("FordST", 400, "Ford")
             ddt.Rows.Add("Note", 1000, "Nissan")
             ddt.Rows.Add("Mazda3", 1000, "Mazda")
-
-            Dim ds As New DataSet("CM")
+            Dim ds As DataSet = New DataSet("CM")
             ds.Tables.Add(mdt)
             ds.Tables.Add(ddt)
-            Dim dr As New DataRelation("CompanyModels", mdt.Columns("Name"), ddt.Columns("CompanyName"))
+            Dim dr As DataRelation = New DataRelation("CompanyModels", mdt.Columns("Name"), ddt.Columns("CompanyName"))
             ds.Relations.Add(dr)
-
             Return ds
         End Function
+
         Private Sub UpdateDetailSource()
-            Dim dv As New DataView(DataSet.Tables("Models"))
+            Dim dv As DataView = New DataView(DataSet.Tables("Models"))
             dv.RowFilter = String.Format("CompanyName = '{0}'", CurrentRow("Name"))
             DetailSource = DataSet.Tables("Models")
         End Sub
 
         Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
         Private Sub RaisePropertyChanged(ByVal propName As String)
             RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propName))
         End Sub
